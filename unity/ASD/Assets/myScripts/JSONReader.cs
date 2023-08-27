616dd6582d8a90d7a46db3ea24917dc0979e603f
red-pencil @ laptop win <hx.4700.00@gmail.com>
2023-08-26 21:47:19 -0400

    refine json
    refine json read and write

--- unity/ASD/Assets/myScripts/JSONReader.cs
+++ unity/ASD/Assets/myScripts/JSONReader.cs
@@ -7,7 +7,9 @@ public class JSONReader : MonoBehaviour
     void Start()
     {
         Employees employeesInJson = JsonUtility.FromJson<Employees>(jsonFile.text);
- 
+
+        Debug.Log(employeesInJson.employees[0].firstName);
+
         foreach (Employee employee in employeesInJson.employees)
         {
             Debug.Log("Found employee: " + employee.firstName + " " + employee.lastName);
---
+++ unity/ASD/Assets/myScripts/SaveChildData.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SaveChildData : MonoBehaviour
+{
+    public GameObject name_input_field, age_input_field, gender_input_field, like_input_field, dislike_input_field, notes_input_field;
+
+    [SerializeField] private ChildrenInfo childInfoArray = new ChildrenInfo();
+    
+    public void SaveIntoJson() {
+        
+        ChildInfo childInfoItem = new ChildInfo();
+        BasicInfo basicInfoItem = new BasicInfo();
+
+        childInfoItem.id = childInfoArray.childInfo.Count;
+        //childInfoItem.id = 99;
+        
+        basicInfoItem.name = name_input_field.GetComponent<TMP_InputField>().text;
+        basicInfoItem.age = int.Parse(age_input_field.GetComponent<TMP_InputField>().text);
+        basicInfoItem.gender = gender_input_field.GetComponent<TMP_InputField>().text;
+        basicInfoItem.like = like_input_field.GetComponent<TMP_InputField>().text;
+        basicInfoItem.dislike = dislike_input_field.GetComponent<TMP_InputField>().text;
+        basicInfoItem.notes = notes_input_field.GetComponent<TMP_InputField>().text;
+        
+
+
+        childInfoItem.basicInfo.Add(basicInfoItem);
+
+        Debug.Log(childInfoItem.id);
+        childInfoArray.childInfo.Add(childInfoItem);
+        string children = JsonUtility.ToJson(childInfoArray);
+        
+        System.IO.File.WriteAllText(Application.persistentDataPath + "/ChildrenData.json", children);
+   }
+
+}
+
+[System.Serializable]
+public class ChildrenInfo {
+    public List<ChildInfo> childInfo = new List<ChildInfo>();
+}
+
+[System.Serializable]
+public class ChildInfo {
+    public int id;
+    public List<BasicInfo> basicInfo = new List<BasicInfo>();
+}
+
+[System.Serializable]
+public class BasicInfo {
+    public string name;
+    public int age;
+    public string gender;
+    public string like;
+    public string dislike;
+    public string notes;
+}
